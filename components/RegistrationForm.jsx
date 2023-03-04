8335f0c42c5e06a9184145d2fc7a476d948df0b3
Bolun Thompson <abolunthompson@gmail.com>
2023-03-04 13:08:24 -0800

    fix registration form size

---
+++ components/RegistrationForm.jsx
@@ -0,0 +1,21 @@
+import { Box, Text, Image } from "rebass";
+import { useColorMode } from "theme-ui";
+import theme from "./theme";
+
+const RegistrationForm = () => {
+  return (
+    <div style={{ height: "80vh" }}>
+      <iframe
+        class="airtable-embed"
+        src="https://airtable.com/embed/shrya40Trxq3m2A8J?backgroundColor=yellow"
+        frameborder="0"
+        onmousewheel=""
+        width="100%"
+        height="100%"
+        style={{ background: "transparent", border: "1px solid #ccc" }}
+      ></iframe>
+    </div>
+  );
+};
+
+export default RegistrationForm;
